7717e3776ed493f0e5fe94245ed278dc00a0a737
Larry <66905431+larry-oj@users.noreply.github.com>
2023-06-27 00:44:01 +0300

    Casting pointer and angle

---
+++ Assets/Scripts/PlayerCasting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCasting : MonoBehaviour
+{
+    private Camera _mainCamera;
+    private Vector3 _mousePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainCamera = GameObject.FindGameObjectWithTag("MainCamera")
+            .GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        var diff = _mousePosition - transform.position;
+        var rotZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg - 90;
+        
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+    }
+}
